1f8d0af2ccac3872d99db7e6313983837f2fa356
NatureStone <59265784+naturestone@users.noreply.github.com>
2020-04-05 11:30:17 +0200

    Add files via upload

---
+++ SandboxApplication.java
@@ -0,0 +1,39 @@
+/**
+ * SandboxApplication Java Sample Program
+ * 
+ * Voraussetzung: ein Java Developer Kit ist installiert
+ * 
+ * Linux (Debian/Ubuntu): sudo apt-get install openjdk-8-jdk  
+ *
+ * VS Code: Java Extension Pack (Microsoft)
+ *   Strg + ',' ==> "java.home"
+ */
+
+import java.util.*;
+
+public class SandboxApplication {
+
+  public static void main(final String args[]) {
+    System.out.println("\nApplication Class: " + SandboxApplication.class.getName());
+    
+    final SandboxApplication app = new SandboxApplication();
+    app.run(args);
+  }
+
+  public void run(final String args[]) {
+    System.out.println("Enter method run()");
+
+    Hashtable<String, String> ht = new Hashtable<String, String>();
+    ht.put("TOR1", "Manuel Neuer");
+    ht.put("TOR2", "Sven Ulreich");
+    ht.put("STURM1", "Thomas Müller");
+    ht.put("STURM2", "Robert Lewandowski");
+    
+    Enumeration names = ht.keys();
+    while (names.hasMoreElements()) {
+      String key = (String) names.nextElement();
+      System.out.println(key + ": " + ht.get(key));
+    }
+  }
+
+}
